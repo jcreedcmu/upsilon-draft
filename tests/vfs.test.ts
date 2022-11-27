c8a7a0afeb0b3e32b347b2f6c321041ab78cec31
Jason Reed <jcreed@gmail.com>
2022-11-27 15:48:08 -0500

    Found another red test

--- tests/vfs.test.ts
+++ tests/vfs.test.ts
@@ -15,6 +15,21 @@ const fs = (() => {
   return fs;
 })();
 
+
+const fs2 = (() => {
+  let fs = mkFs();
+  [fs,] = insertPlans(fs, SpecialId.root, [
+    {
+      t: 'dir', name: 'dir', forceId: 'dir', contents: [
+        { t: 'virtual', id: 'vroot' },
+        testFile('foo_a'),
+      ]
+    }
+  ]);
+  return fs;
+})();
+
+
 describe('virtual filesystem', () => {
   test('should work correctly', () => {
     expect(getItem(fs, '_gen_vroot').contents.length > 0).toBe(true);
@@ -27,5 +42,18 @@ describe('virtual filesystem', () => {
         'dir-3',
         '\x81\x95\x83\x9D\x95'
       ]);
+
+    expect(getFullContents(fs, '_root').map(x => x.name))
+      .toEqual([
+        'virtual',
+        'dir',
+      ]);
+
+    expect(getFullContents(fs2, 'dir').map(x => x.name))
+      .toEqual([
+        'virtual',
+        'foo_a',
+      ]);
+
   });
 });
