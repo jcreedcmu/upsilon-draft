1391bbcaf15900bbae99f1b6ab00935a1b26ba8d
Jason Reed <jcreed@gmail.com>
2022-12-26 13:05:34 -0500

    Correct relative path

--- src/core/assets.ts
+++ src/core/assets.ts
@@ -6,7 +6,7 @@ let assets: Assets | undefined = undefined;
 
 export async function initAssets() {
   assets = {
-    surnames: (await (await fetch('../assets/surnames.txt')).text())
+    surnames: (await (await fetch('assets/surnames.txt')).text())
       .split('\n').map(x => x.toLowerCase())
   };
 }
