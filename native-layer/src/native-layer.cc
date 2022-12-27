3d1892d1c255255f8be79c06df5b39f1683abf0b
Jason Reed <jcreed@gmail.com>
2022-12-27 15:19:57 -0500

    Collect stuff together in a class

--- native-layer/src/native-layer.cc
+++ native-layer/src/native-layer.cc
@@ -3,43 +3,53 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 
-SDL_Window *window = nullptr;
-SDL_GLContext context;
+class NativeLayer {
+private:
+  SDL_Window *window;
+  SDL_GLContext context;
+
+public:
+  NativeLayer() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
+                        SDL_GL_CONTEXT_PROFILE_CORE);
+
+    static const int width = 800;
+    static const int height = 600;
+
+    this->window =
+        SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                         width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    this->context = SDL_GL_CreateContext(window);
+  }
+
+  void finish() {
+    SDL_GL_DeleteContext(this->context);
+    SDL_DestroyWindow(this->window);
+    SDL_Quit();
+  }
+};
+
+NativeLayer *nativeLayer = nullptr;
 
 Napi::Value initGame(const Napi::CallbackInfo &info) {
   Napi::Env env = info.Env();
-
-  SDL_Init(SDL_INIT_VIDEO);
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-  SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
-  SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
-  static const int width = 800;
-  static const int height = 600;
-
-  window =
-      SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
-  context = SDL_GL_CreateContext(window);
-
+  nativeLayer = new NativeLayer();
   return env.Null();
 }
 
 Napi::Value finishGame(const Napi::CallbackInfo &info) {
   Napi::Env env = info.Env();
-
-  SDL_GL_DeleteContext(context);
-  if (window != nullptr)
-    SDL_DestroyWindow(window);
-  SDL_Quit();
-
+  nativeLayer->finish();
   return env.Null();
 }
 
