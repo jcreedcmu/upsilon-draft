54077af0c357cd59d07dc50f1e5156bb8decc879
Jason Reed <jcreed@gmail.com>
2022-12-27 15:34:02 -0500

    Can wrap C++ object in js

--- native-layer/src/native-layer.cc
+++ native-layer/src/native-layer.cc
@@ -3,6 +3,68 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
 
+class ObjectWrapDemo : public Napi::ObjectWrap<ObjectWrapDemo> {
+ public:
+  ObjectWrapDemo(const Napi::CallbackInfo&);
+  Napi::Value Greet(const Napi::CallbackInfo&);
+
+  static Napi::Function GetClass(Napi::Env);
+
+ private:
+  std::string _greeterName;
+};
+
+ObjectWrapDemo::ObjectWrapDemo(const Napi::CallbackInfo& info)
+    : ObjectWrap(info) {
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 1) {
+    Napi::TypeError::New(env, "Wrong number of arguments")
+        .ThrowAsJavaScriptException();
+    return;
+  }
+
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "You need to name yourself")
+        .ThrowAsJavaScriptException();
+    return;
+  }
+
+  this->_greeterName = info[0].As<Napi::String>().Utf8Value();
+}
+
+Napi::Value ObjectWrapDemo::Greet(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+
+  if (info.Length() < 1) {
+    Napi::TypeError::New(env, "Wrong number of arguments")
+        .ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  if (!info[0].IsString()) {
+    Napi::TypeError::New(env, "You need to introduce yourself to greet")
+        .ThrowAsJavaScriptException();
+    return env.Null();
+  }
+
+  Napi::String name = info[0].As<Napi::String>();
+
+  printf("Hello %s\n", name.Utf8Value().c_str());
+  printf("I am %s\n", this->_greeterName.c_str());
+
+  return Napi::String::New(env, this->_greeterName);
+}
+
+Napi::Function ObjectWrapDemo::GetClass(Napi::Env env) {
+  return DefineClass(
+      env,
+      "ObjectWrapDemo",
+      {
+          ObjectWrapDemo::InstanceMethod("greet", &ObjectWrapDemo::Greet),
+      });
+}
+
 class NativeLayer {
 private:
   SDL_Window *window;
@@ -64,6 +126,8 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
   exports.Set("hello", Napi::Function::New(env, Hello));
   exports.Set("init", Napi::Function::New(env, initGame));
   exports.Set("finish", Napi::Function::New(env, finishGame));
+
+  exports.Set(Napi::String::New(env, "ObjectWrapDemo"), ObjectWrapDemo::GetClass(env));
 
   return exports;
 }
--- sdl-game/index.js
+++ sdl-game/index.js
@@ -1,5 +1,10 @@
 const nativeLayer = require('native-layer');
+const ObjectWrapDemo = nativeLayer.ObjectWrapDemo;
 
 nativeLayer.init();
 console.log(nativeLayer.hello());
+
+const owd = new ObjectWrapDemo('alice');
+owd.greet('bob');
+
 setTimeout(() => nativeLayer.finish(), 1000);
