f6577b5a7c868501eb88949b7731b365b72058d5
Jason Reed <jcreed@gmail.com>
2023-01-05 14:26:21 -0500

    Correctly post-processing in native

--- sdl-game/src/index.ts
+++ sdl-game/src/index.ts
@@ -33,21 +33,27 @@ nativeLayer.configShaders(programSynth.programId());
 nat.glUniform2f(programSynth.getUniformLocation("u_offset"), 0, 0);
 nat.glUniform2f(programSynth.getUniformLocation("u_size"), width, height);
 nat.glUniform2f(programSynth.getUniformLocation("u_viewport_size"), width, height);
-nat.glUniform1i(programSynth.getUniformLocation("u_screenTexture"), TextureUnit.FB);
-nat.glUniform1f(programSynth.getUniformLocation("u_time"), 0.0);
-nat.glUniform1f(programSynth.getUniformLocation("u_beamScale"), 1.0);
-nat.glUniform1f(programSynth.getUniformLocation("u_fade"), 1.0);
-nat.glUniform2f(programSynth.getUniformLocation("windowSize"), screen_width, screen_height);
 
 const programTexture = new nat.Program(shader.vertex, shader.fragmentTexture);
 nativeLayer.configShaders(programTexture.programId());
 
 const u_sampler = programTexture.getUniformLocation('u_sampler');
 
-const programPost = new nat.Program(shader.vertex, shader.fragPost);
+const programPost = new nat.Program(shader.vertexFlip, shader.fragPost);
 nativeLayer.configShaders(programPost.programId());
-
+nat.glUniform2f(programPost.getUniformLocation("u_offset"), (width - screen_width) / 2, (height - screen_height) / 2);
+nat.glUniform2f(programPost.getUniformLocation("u_size"), screen_width, screen_height);
+nat.glUniform2f(programPost.getUniformLocation("u_viewport_size"), width, height);
+nat.glUniform1i(programPost.getUniformLocation("u_screenTexture"), TextureUnit.FB);
+nat.glUniform1f(programPost.getUniformLocation("u_beamScale"), 1.0);
+nat.glUniform1f(programPost.getUniformLocation("u_fade"), 1.0);
+nat.glUniform2f(programPost.getUniformLocation("windowSize"), screen_width, screen_height);
 fbTexture.bind(TextureUnit.FB);
+
+const progStart = Date.now();
+function time(): number {
+  return (Date.now() - progStart) / 1000;
+}
 
 while (nativeLayer.pollEvent()) {
   nativeLayer.clear();
@@ -60,13 +66,11 @@ while (nativeLayer.pollEvent()) {
 
   // Draw screen postprocessing
   programPost.use();
-  nat.glUniform2f(programPost.getUniformLocation("u_offset"), (width - screen_width) / 2, (height - screen_height) / 2);
-  nat.glUniform2f(programPost.getUniformLocation("u_size"), screen_width, screen_height);
-  nat.glUniform2f(programPost.getUniformLocation("u_viewport_size"), width, height);
+  nat.glUniform1f(programPost.getUniformLocation("u_time"), time());
   nativeLayer.drawTriangles();
 
   // Draw power button
-  const buttonTexture = (Math.floor(Date.now() / 1000) % 2 == 0) ? button1 : button2;
+  const buttonTexture = (Math.floor(time()) % 2 == 0) ? button1 : button2;
   programTexture.use();
   nat.glUniform2f(programTexture.getUniformLocation("u_offset"), width - 100, height - 75);
   nat.glUniform2f(programTexture.getUniformLocation("u_size"), 100, 75);
--- sdl-game/src/shaders.ts
+++ sdl-game/src/shaders.ts
@@ -14,7 +14,23 @@ uniform vec2 u_viewport_size;
 void main() {
   // negating y-coord means (0,0) at top left of screen
   vec2 pos = (i_uv * u_size + u_offset) / u_viewport_size;
-  gl_Position = vec4(2. * pos.x - 1., - (2. * pos.y - 1.), 0., 1.);
+  gl_Position = vec4(2. * pos.x - 1., -(2. * pos.y - 1.), 0., 1.);
+  v_uv = i_uv;
+};
+`
+
+export const vertexFlip = `
+#version 300 es
+in vec2 i_uv;
+out vec2 v_uv;
+
+uniform vec2 u_offset;
+uniform vec2 u_size;
+uniform vec2 u_viewport_size;
+
+void main() {
+  vec2 pos = (i_uv * u_size + u_offset) / u_viewport_size;
+  gl_Position = vec4(2. * pos.x - 1., 2. * pos.y - 1., 0., 1.);
   v_uv = i_uv;
 };
 `
