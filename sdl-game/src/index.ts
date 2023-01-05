7708db8d7b3643b9640061c9c4677a2681f6b23c
Jason Reed <jcreed@gmail.com>
2023-01-05 14:04:48 -0500

    Draw framebuffer texture

--- sdl-game/src/index.ts
+++ sdl-game/src/index.ts
@@ -16,7 +16,6 @@ enum TextureUnit {
   BUTTON,
 }
 
-console.log(shader.fragPost);
 const button1 = new nat.Texture();
 button1.loadFile('public/assets/button-down.png');
 const button2 = new nat.Texture();
@@ -31,8 +30,8 @@ fb.unbind();
 
 const programX = new nat.Program(shader.vertex, shader.fragmentSynthetic);
 nativeLayer.configShaders(programX.programId());
-nat.glUniform2f(programX.getUniformLocation("u_offset"), (width - screen_width) / 2, (height - screen_height) / 2);
-nat.glUniform2f(programX.getUniformLocation("u_size"), screen_width, screen_height);
+nat.glUniform2f(programX.getUniformLocation("u_offset"), 0, 0);
+nat.glUniform2f(programX.getUniformLocation("u_size"), width, height);
 nat.glUniform2f(programX.getUniformLocation("u_viewport_size"), width, height);
 
 const programTexture = new nat.Program(shader.vertex, shader.fragmentTexture);
@@ -43,18 +42,29 @@ nat.glUniform2f(programTexture.getUniformLocation("u_viewport_size"), width, hei
 
 const u_sampler = programTexture.getUniformLocation('u_sampler');
 nativeLayer.configShaders(programTexture.programId());
-nat.glUniform1i(u_sampler, TextureUnit.BUTTON);
 
 while (nativeLayer.pollEvent()) {
   nativeLayer.clear();
 
-  //fb.bind();
+  fb.bind();
   programX.use();
   nativeLayer.drawTriangles();
-  //fb.unbind();
+  fb.unbind();
+
+  programTexture.use();
+  nat.glUniform2f(programX.getUniformLocation("u_offset"), (width - screen_width) / 2, (height - screen_height) / 2);
+  nat.glUniform2f(programX.getUniformLocation("u_size"), screen_width, screen_height);
+  nat.glUniform2f(programX.getUniformLocation("u_viewport_size"), width, height);
+  nat.glUniform1i(u_sampler, TextureUnit.FB);
+  fbTexture.bind(TextureUnit.FB);
+  nativeLayer.drawTriangles();
 
   const buttonTexture = (Math.floor(Date.now() / 1000) % 2 == 0) ? button1 : button2;
   programTexture.use();
+  nat.glUniform2f(programTexture.getUniformLocation("u_offset"), width - 100, height - 75);
+  nat.glUniform2f(programTexture.getUniformLocation("u_size"), 100, 75);
+  nat.glUniform2f(programTexture.getUniformLocation("u_viewport_size"), width, height);
+  nat.glUniform1i(u_sampler, TextureUnit.BUTTON);
   buttonTexture.bind(TextureUnit.BUTTON);
   nativeLayer.drawTriangles();
 
